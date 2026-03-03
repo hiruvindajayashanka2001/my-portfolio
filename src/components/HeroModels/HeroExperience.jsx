fde46ab43a00b71ffc34494cadf6ded59b7f710f
M A H Jayashanka <hiruvindajayashanka2001@gmail.com>
2026-03-04 00:38:31 +0530

    implement 3d model rendering part

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,16 +1,15 @@
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { useMediaQuery } from "react-responsive";
-
-const isTablet = useMediaQuery({ query: '(max-width: 1024px)'});
-const isMobile = useMediaQuery({ query: '(max-width: 768px)'});
-
+import Room from "./Room.jsx"
+import HeroLights from "./HeroLights.jsx";
 
 const HeroExperience = () => {
+    const isTablet = useMediaQuery({ query: '(max-width: 1024px)'});
+    const isMobile = useMediaQuery({ query: '(max-width: 768px)'});
+
     return (
         <Canvas camera={{position: [0, 0, 15], fov: 45}}>
-            <ambientLight  intensity={0.2} color="#1a1a40"/>
-            <directionalLight position={[5,5,5]} intensity={1}/>
             <OrbitControls
                 enablePan={false}
                 enableZoom={!isTablet}
@@ -19,12 +18,11 @@ const HeroExperience = () => {
                 minPolarAngle={Math.PI/5}
                 maxPolarAngle={Math.PI/2}
             />
+            <HeroLights/>
+            <group scale={isMobile? 0.7:1} position={[0, -3.5, 0]} rotation={[0.2, -Math.PI/3, 0]}>
+                <Room/>
+            </group>
 
-            <mesh>
-                <boxGeometry args={[1, 1, 1]}/>
-                <meshStandardMaterial color="teal"/>
-
-            </mesh>
         </Canvas>
     )
 }
