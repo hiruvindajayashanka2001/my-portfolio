e20b651c1c22d668e522438db7aa6be807c268c3
M A H Jayashanka <hiruvindajayashanka2001@gmail.com>
2026-03-04 23:59:24 +0530

    import about section in the app.js

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import react from 'react'
 import Hero from './sections/Hero'
+import About from './sections/About'
 
 const app = () => {
     return (
         <>
             <Hero/>
+            <About/>
         </>
     )
 }
